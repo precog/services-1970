258a92d0c13a347e757e78d4e889e455ad6b12fd
John A. De Goes <john@degoes.net>
2011-05-14 16:07:33 -0600

    tighten up imports

--- analytics/src/test/scala/com/reportgrid/analytics/AggregationEngineSpec.scala
+++ analytics/src/test/scala/com/reportgrid/analytics/AggregationEngineSpec.scala
@@ -6,7 +6,6 @@ import blueeyes.core.http.HttpStatusCodes._
 import blueeyes.core.service.test.BlueEyesServiceSpecification
 import blueeyes.concurrent.test.FutureMatchers
 import blueeyes.concurrent.Duration
-import blueeyes.concurrent.Duration._
 import MimeTypes._
 
 import blueeyes.json.JsonAST.{JValue, JObject, JField, JString, JNothing, JArray}
@@ -17,7 +16,7 @@ import blueeyes.persistence.mongo.{Mongo, MockMongo}
 import net.lag.configgy.{Configgy, Config, ConfigMap}
 import net.lag.logging.Logger
 
-import org.specs._
+import org.specs.{Specification, ScalaCheck}
 import org.specs.specification.PendingUntilFixed
 import org.scalacheck._
 import Gen._
