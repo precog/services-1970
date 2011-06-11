c00dbe14a0f9e7850e1598904a8979965ffb51e0
Kris Nuttycombe <kris@hylotech.com>
2011-06-11 10:31:43 -0600

    Upgraded to scalaz 6.0.1 final

--- project/Build.scala
+++ project/Build.scala
@@ -29,7 +29,7 @@ object ServicesBuild extends Build {
     val analyticsSettings = Seq( 
       resolvers ++= Seq("Scala-Tools Maven2 Snapshots Repository" at "http://scala-tools.org/repo-snapshots"),
       libraryDependencies ++= Seq(
-        "org.scalaz"              %% "scalaz-core" % "6.0.1-SNAPSHOT",
+        "org.scalaz"              %% "scalaz-core" % "6.0.1",
         "org.scala-tools.testing" %% "specs"       % "1.6.8"  % "test",
         "org.scala-tools.testing" %% "scalacheck"  % "1.8"    % "test"),
       mainClass := Some("com.reportgrid.analytics.AnalyticsServer")
