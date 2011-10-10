94560f8c5e7781e61b0876a1c56d1b4deeb1086b
Kris Nuttycombe <kris@hylotech.com>
2011-10-10 11:32:40 -0600

    Added build for billing project.

--- project/Build.scala
+++ project/Build.scala
@@ -29,12 +29,14 @@ object ServicesBuild extends Build {
       settings = serviceSettings ++ Seq(libraryDependencies += "joda-time" % "joda-time" % "1.6.2")
     )
 
+
     val analyticsSettings = serviceSettings ++ Seq( 
       libraryDependencies ++= Seq(
         "joda-time"               % "joda-time"    % "1.6.2",
         "org.scalaz"              %% "scalaz-core" % "6.0.2",
         "org.scala-tools.testing" %% "specs"       % "1.6.9"  % "test",
-        "org.scala-tools.testing" %% "scalacheck"  % "1.9"    % "test"),
+        "org.scala-tools.testing" %% "scalacheck"  % "1.9"    % "test"
+      ),
       mainClass := Some("com.reportgrid.analytics.AnalyticsServer"),
       jarName in assembly := "analytics-v1.jar"
     )
@@ -42,34 +44,29 @@ object ServicesBuild extends Build {
     val analytics = Project("analytics", file("analytics"), settings = sbtassembly.Plugin.assemblySettings ++ analyticsSettings) dependsOn(common) dependsOnAlt (blueeyes(base)) dependsOnAlt(client(base))
 
 
-    val yggdrasilSettings = serviceSettings ++ sbtassembly.Plugin.assemblySettings ++ Seq(
-      resolvers ++= Seq(
-        //"riptano" at "http://mvn.riptano.com/content/repositories/public",
-        //"Scale7 Maven Repo" at "https://github.com/s7/mvnrepo/raw/master"
-        "Cloudera"       at "https://repository.cloudera.com/content/groups/cloudera-repos",
-        "Java.net"       at "http://download.java.net/maven/2/",
-        "HBase Thrift"   at "http://people.apache.org/~rawson/repo"
-      ),
+    val billingSettings = serviceSettings ++ sbtassembly.Plugin.assemblySettings ++ Seq(
       libraryDependencies ++= Seq(
-        //"org.scale7" % "scale7-pelops" % "1.2-0.8.x-SNAPSHOT",
-        "org.apache.hbase"        % "hbase"        % "0.90.1-cdh3u0",
-        "org.slf4j"               % "slf4j-api"    % "1.6.1", 
-        "joda-time"               % "joda-time"    % "1.6.2",
-        "org.scalaz"              %% "scalaz-core" % "6.0.2"
+        "org.scala-tools.testing" %% "specs"       % "1.6.9"  % "test",
+        "org.scala-tools.testing" %% "scalacheck"  % "1.9"    % "test"
       ),
-      mainClass := Some("com.reportgrid.yggdrasil.Yggdrasil")
+      mainClass := Some("com.reportgrid.billing.BillingServer"),
+      jarName in assembly := "billing-v1.jar"
     )
+
+    val billing = Project("billing", file("billing"), settings = billingSettings) dependsOnAlt blueeyes(base)
+
 
     val jessupSettings = serviceSettings ++ sbtassembly.Plugin.assemblySettings ++ Seq(
       libraryDependencies ++= Seq(
         "org.dspace.dependencies" % "dspace-geoip" % "1.2.3",
         "org.scala-tools.testing" %% "specs"       % "1.6.9"  % "test",
-        "org.scala-tools.testing" %% "scalacheck"  % "1.9"    % "test"),
-      mainClass := Some("com.reportgrid.jessup.Server"))
-
-    val yggdrasil = Project("yggdrasil", file("yggdrasil"), settings = yggdrasilSettings ) dependsOn(common) dependsOnAlt (blueeyes(base))
+        "org.scala-tools.testing" %% "scalacheck"  % "1.9"    % "test"
+      ),
+      mainClass := Some("com.reportgrid.jessup.Server")
+    )
 
     val jessup = Project("jessup", file("jessup"), settings = jessupSettings) dependsOnAlt blueeyes(base)
+
 
     val benchmarkSettings = serviceSettings ++ Seq(
       libraryDependencies += "org.scala-tools.testing" %% "scalacheck"  % "1.9" % "compile",
@@ -78,7 +75,9 @@ object ServicesBuild extends Build {
 
     val benchmark = Project("benchmark", file("benchmark"), settings = benchmarkSettings ++ sbtassembly.Plugin.assemblySettings) dependsOn(common) dependsOnAlt(blueeyes(base)) dependsOnAlt(client(base))
 
-    val services = Project("services", file(".")) aggregate (common, analytics, benchmark, yggdrasil) 
-    common :: analytics :: benchmark :: yggdrasil :: jessup :: services :: Nil
+
+    val services = Project("services", file(".")) aggregate (common, analytics, benchmark, billing, jessup) 
+
+    common :: analytics :: benchmark :: billing :: jessup :: services :: Nil
   }
 }
