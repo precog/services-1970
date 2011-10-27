f6259d5e79f926f3776ded1c2978c08d7ef10a19
Kris Nuttycombe <kris@reportgrid.com>
2011-10-27 13:28:34 -0600

    Version bump after 1.2.1

--- project/Build.scala
+++ project/Build.scala
@@ -6,7 +6,7 @@ import sbt.NameFilter._
 
 object ServicesSettings {
   val buildOrganization = "com.reportgrid"
-  val buildVersion = "1.2.1"
+  val buildVersion = "1.2.2-SNAPSHOT"
   val buildScalaVersion = "2.9.1"
   
   val serviceSettings = Defaults.defaultSettings ++ Seq (
