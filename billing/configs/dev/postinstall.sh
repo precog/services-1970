6067f55b3607f73e1ccda08c85131136a2adabd3
Derek Chen-Becker <derek@reportgrid.com>
2011-11-28 06:57:06 -0700

    Small change to fix postinstall bug

--- billing/configs/dev/postinstall.sh
+++ billing/configs/dev/postinstall.sh
@@ -3,13 +3,22 @@
 # Reload the upstart config
 initctl reload-configuration
 
+# Keep monit from interrupting us
+/etc/init.d/monit stop
+
 # Stop and start the service
 stop billing-v1
 sleep 5
-start billing-v1
+
+if ! RESULT=`start billing-v1 2>&1` > /dev/null ; then
+        if echo "$RESULT" | grep -v "already running" > /dev/null ; then
+		exit 1
+        fi
+fi
+
 
 # Restart monit to pick up changes
-/etc/init.d/monit restart
+/etc/init.d/monit start
 
 # Wait 30 seconds for startup, then test the health URL
 sleep 30
