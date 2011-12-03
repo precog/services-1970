5966a7d8d1bad05e679151a167c7bdf80501c9c4
Derek Chen-Becker <derek@reportgrid.com>
2011-12-02 22:42:18 -0700

    Tweaked postinstall for analytics

--- analytics/configs/dev/postinstall.sh
+++ analytics/configs/dev/postinstall.sh
@@ -1,19 +1,37 @@
 #!/bin/bash
 
+set -e
+
 # Reload the upstart config
 initctl reload-configuration
+
+# Keep monit from interrupting us
+if /etc/init.d/monit stop; then
+	echo "Monit stopped unhappy"
+fi
 
 # Stop and start the service
 stop analytics-v1
 sleep 5
-start analytics-v1
+
+if ! RESULT=`start analytics-v1 2>&1` > /dev/null ; then
+        if echo "$RESULT" | grep -v "already running" > /dev/null ; then
+		echo "Failure: $RESULT"
+		exit 1
+        fi
+fi
+
 
 # Restart monit to pick up changes
-/etc/init.d/monit restart
+if /etc/init.d/monit start; then
+	echo "Monit started unhappy";
+fi
 
 # Wait 30 seconds for startup, then test the health URL
 sleep 30
 
-set -e
+echo "Checking health"
+curl -v -f -G "http://localhost:30020/blueeyes/services/analytics/v1/health"
+echo "Completed health check"
 
-curl -f -G "http://localhost:30020/blueeyes/services/analytics/v1/health"
+exit 0
