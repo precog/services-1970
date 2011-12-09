749edc3d2954a6755129324644aa4ac700ef4fbe
Kris Nuttycombe <kris@reportgrid.com>
2011-12-09 11:41:34 -0700

    Version bump for 1.3.3 final.

--- project/Build.scala
+++ project/Build.scala
@@ -36,7 +36,7 @@ object ServicesBuild extends Build {
     val common = Project("common", file("common"), settings = commonSettings) dependsOnAlt(blueeyes(base)) 
 
     val analyticsSettings = serviceSettings ++ Seq( 
-      version      := "1.3.3-SNAPSHOT",
+      version      := "1.3.3",
       libraryDependencies ++= Seq(
         "joda-time"               % "joda-time"           % "1.6.2",
         "ch.qos.logback"          % "logback-classic"     % "1.0.0",
