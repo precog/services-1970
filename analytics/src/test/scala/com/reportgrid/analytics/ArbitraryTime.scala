d354f23e0bdaf7b2348adb0cc256d15c4e794bd1
Kris Nuttycombe <kris@reportgrid.com>
2011-12-15 16:14:34 -0700

    Improved arbitrary time generation to cover a large time range.

--- analytics/src/test/scala/com/reportgrid/analytics/ArbitraryTime.scala
+++ analytics/src/test/scala/com/reportgrid/analytics/ArbitraryTime.scala
@@ -6,11 +6,36 @@ import org.scalacheck.Gen._
 import org.scalacheck.Prop._
 import Periodicity._
 import Arbitrary._
+import scalaz.Scalaz._
+import scalaz.Monad
 
 trait ArbitraryTime {
   val genTimeClock: Clock
 
-  val genTime = for (i <- choose(0, 1000 * 60 * 60 * 48)) yield genTimeClock.now().plus(i).toInstant
+  implicit object GenMonad extends Monad[Gen] {
+    def pure[A](a: => A) = value(a)
+    def bind[A, B](m: Gen[A], f: A => Gen[B]) = m.flatMap(f)
+  }
+
+  val genTimePeriodicities: Map[Periodicity, Int] = Map(
+    Second -> 60,
+    Minute -> 60,
+    Hour -> 24,
+    Day -> 31,
+    Week -> 52,
+    Month -> 12,
+    Year -> 3
+  )
+
+  val genTime = {
+    for {
+      periodicities <- pick(3, genTimePeriodicities)
+      offsets <- periodicities map { case (periodicity, max) => choose(0, max).map(periodicity.jodaPeriod(_).get) } sequence
+    } yield {
+      offsets.foldLeft(genTimeClock.now()) { (date, offset) => date.plus(offset) } toInstant
+    }
+  }
+
   implicit val arbTime = Arbitrary(genTime)
 
   def genPeriodicity(pers: Periodicity*) = oneOf[Periodicity](pers)
